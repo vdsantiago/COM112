dd226829033aec7fca057d9b704181593d23a380
vdsantiago <50530125+vdsantiago@users.noreply.github.com>
2020-05-04 23:40:23 -0300

    Update com112_main.c

--- Lista4/com112_main.c
+++ Lista4/com112_main.c
@@ -2,34 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 #include "com112_sort.h"
-#include "com112_sort.c"
-//#include "com112_file.h"
+#include "com112_file.h"
 
-void menu()
+void relatorio(double time, int comparison, int movement, int flag)
 {
-    int n = 0;
-    printf("Escolha o tamanho do vetor que você deseja criar\n");
-    scanf("%d", &n);
-
-    int *v, *r, i, vet[n], reg[2];
-        
-    for (i = 0; i < n; i++)
+    if (flag == 0)
     {
-        vet[i] = rand() % 50;
+        printf("Método Bubble Sort:\n");
     }
-
-    printf("Vetor preenchido automaticamente:\n");
-    printf("[");
-    for (i = 0; i < n; i++)
-    {
-        printf(" %d", vet[i]);
+    else if(flag == 1){
+        printf("Método Selection Sort:\n");
     }
-    printf(" ]\n\n");
+    else{
+        printf("Método Insertion Sort:\n");
+    }
+    
+    printf("    Tempo de execução: %fs\n", time);
+    printf("    Número de comparações: %d\n",comparison);
+    printf("    Número de movimentações: %d\n", movement);
+    printf("\n");
+}
 
-    v = vet;
-    r = reg;
-
-    int opc = 0;
+int menu()
+{
+    int opc;
 
     printf("Selecione o método de ordenação que deseja utilizar:\n");
     printf("Digite (1) para Bubble Sort\n");
@@ -38,35 +34,68 @@ void menu()
 
     scanf("%d", &opc);
 
-    switch (opc)
+    return opc;
+}
+
+void generateArray(int *array, int n){
+    int i;
+
+    srand(time(NULL));
+    for (i = 0; i < n; i++)
     {
-        case 1:
-            bubble_sort(v, r, n);
-            printf("%d ", r[0]);
-            printf("%d", r[1]);
-            break;
-                
-        case 2:
-            selection_sort(v, r, n);
-            printf("%d ", r[0]);
-            printf("%d", r[1]);
-            break;
-                
-        case 3:
-            insertion_sort(v, r, n);
-            printf("%d ", r[0]);
-            printf("%d", r[1]);
-            break;
-            
+        array[i] = rand() % 50;
     }
 }
 
+
 int main()
 {
-    menu();
-}
+    int n, opc, flag;
+    printf("Escolha o tamanho do vetor que você deseja criar\n");
+    scanf("%d", &n);
 
-/*int relatorio()
+    int vet[n];
+    int dataArray[2];
+
+    generateArray(vet, n);
+
+    writeFile(n, vet, 0);
+    opc = menu(n, vet);
+
+    clock_t t;
+
+    switch (opc)
     {
+        case 1:
+            flag = 0;
+            t = clock();
+            bubble_sort(vet, n, dataArray);
+            t = clock() - t;
+            break;
+                
+        case 2:
+            flag = 1;
+            t = clock();
+            selection_sort(vet, n, dataArray);
+            t = clock() - t;
+            break;
+                
+        case 3:
+            flag = 3;
+            t = clock();
+            insertion_sort(vet, n, dataArray);
+            t = clock() - t;
+            break;    
+    }
 
-    }*/
+    double time = ((double)t)/CLOCKS_PER_SEC;
+
+    writeFile(n, vet, 1);
+
+    writeReport(n, dataArray[0], dataArray[1], flag, time);
+
+    relatorio(time, dataArray[0], dataArray[1], flag);
+    
+    return 0;
+
+}
